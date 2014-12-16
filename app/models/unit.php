7bb5c619a4d9f0095719e93bbcf5ebd1699f1b3e
RomeoVargas <r.vargas@klab.com>
2014-12-16 12:37:35 +0800

    Saved changes for testing

---
+++ app/models/unit.php
@@ -0,0 +1,88 @@
+<?php
+class Unit extends AppModel
+{
+    const WARRIOR_CLASS_ID = 1;
+    const ARCHER_CLASS_ID = 2;
+    const CLERIC_CLASS_ID = 3;
+    const MAGE_CLASS_ID = 4;
+
+    public function __construct($row)
+    {
+        parent::__construct($row);
+    }
+
+    public function get($unit_id)
+    {
+        $db = DB::conn();
+        $unit = $db->row('SELECT * FROM unit WHERE id = ?', array($unit_id));
+        $unit['current_lvl'] = $this->current_lvl;
+        $unit['exp_to_go'] = $this->exp_to_go;
+        $unit = $this->getStats($unit);
+        return new self($unit);
+    }
+
+    public function getClass($class_id)
+    {
+        $db = DB::conn();
+        $class = $db->row('SELECT * FROM class WHERE id = ?', array($class_id));
+        return new self($class);
+    }
+
+    public function getStats(array $unit)
+    {
+        $unit_class = $this->getClass($unit['class_id']);
+        $unit['int'] = ($unit['int'] + $unit_class->int_up_per_lvl) * $unit['current_lvl'];
+        $unit['agi'] = ($unit['agi'] + $unit_class->agi_up_per_lvl) * $unit['current_lvl'];
+        $unit['str'] = ($unit['str'] + $unit_class->str_up_per_lvl) * $unit['current_lvl'];
+        $unit['vit'] = ($unit['vit'] + $unit_class->vit_up_per_lvl) * $unit['current_lvl'];
+        $unit['atk'] = $this->getAttack($unit);
+        $unit['def'] = $this->getDefense($unit['vit']);
+        $unit['hp'] =  $this->getHealthPoint($unit['vit'], $unit['current_level']);
+        return $unit;
+    }
+
+    public function getAttack(array $unit)
+    {
+        switch ($unit['class_id'])
+        {
+            case self::WARRIOR_CLASS_ID:
+                $unit_stats = $unit['str'];
+                break;
+            case self::ARCHER_CLASS_ID:
+                $unit_stats = $unit['agi'];
+                break;
+            case self::CLERIC_CLASS_ID:
+            case self::MAGE_CLASS_ID:
+                $unit_stats = $unit['int'];
+                break;
+            default:
+                throw new ClassIdNotFoundException();
+        }
+        $attack = ceil(($unit['current_level']/2) + $unit_stats);
+        return $attack;
+    }
+
+    public function getDefense($vit)
+    {
+        $defense = round(($vit/2) + max(($vit*0.3) * (($vit^2)/150) / 3));
+        return $defense;
+    }
+
+    public function getHealthPoint($vit, $current_level)
+    {
+        $hp = round((($vit*10) + $current_level) + $vit);
+        return $hp;
+    }
+
+    public function getLeaderSkill($unit_id)
+    {
+        $db = DB::conn();
+        $unit_leader_skill = $db->row('SELECT * FROM unit_leader_skill WHERE id = ?', array($this->unit_leader_skill_id));
+        if (!$unit_leader_skill) {
+            return false;
+        }
+        $unit_leader_skill['unit_leader_id'] = $unit_leader_id;
+        return $unit_leader_skill;
+    }
+
+}
--- app/models/unit_storage.php
+++ app/models/unit_storage.php
@@ -1,110 +1,44 @@
 <?php
-class UnitStorage
+class UnitStorage extends AppModel
 {
-    const WARRIOR_CLASS_ID = 1;
-    const ARCHER_CLASS_ID = 2;
-    const CLERIC_CLASS_ID = 3;
-    const MAGE_CLASS_ID = 4;
+    private $player;
+    private $units;
+    private $unit_leader_id;
 
-    private static $player = null;
-
-    public function __construct(Player $player)
+    public function __construct($row)
     {
-        if (is_null(self::$player)) {
-            self::$player = $player;
-        }
+        $this->player = $row;
     }
 
-    public function getClass($class_id)
-    {
-        $db = DB::conn();
-        $class = $db->row('SELECT * FROM class WHERE id = ?', array($class_id));
-        return new self($class);
-    }
-
-    public function getAll()
+    public function getUnits()
     {
         $db = DB::conn();
         $units = array();
-        $rows = $db->rows('SELECT unit_id FROM player_units WHERE player_id = ?', array(self::$player->id));
+        $rows = $db->rows('SELECT * FROM player_units WHERE player_id = ?', array($this->player->id));
         foreach ($rows as $row) {
-            $units[] = $this->getUnitById($row['unit_id']);
+            $row['player'] = $this->player;
+            $unit = new Unit($row);
+            $units[] = $unit->get($row['unit_id']);
         }
         return $units;
     }
 
-    public function getUnitById($unit_id)
+    public function getUnit($unit_id)
     {
         $db = DB::conn();
-        $unit = $db->row('SELECT * FROM player_units p INNER JOIN unit u ON p.unit_id = u.id WHERE p.player_id = ? AND u.id = ?',
-            array(self::$player->id, $unit_id));
-        $unit = $this->getUnitStats($unit);
-        return new self($unit);
-    }
-
-    public function getUnitStats(array $unit)
-    {
-        $unit_class = $this->getClass($unit['class_id']);
-        $unit['int'] = ($unit['int'] + $unit_class->int_up_per_lvl) * $unit['current_lvl'];
-        $unit['agi'] = ($unit['agi'] + $unit_class->agi_up_per_lvl) * $unit['current_lvl'];
-        $unit['str'] = ($unit['str'] + $unit_class->str_up_per_lvl) * $unit['current_lvl'];
-        $unit['vit'] = ($unit['vit'] + $unit_class->vit_up_per_lvl) * $unit['current_lvl'];
-        $unit['atk'] = $this->getAttack($unit);
-        $unit['def'] = $this->getDefense($unit['vit']);
-        $unit['hp'] =  $this->getHealthPoint($unit['vit'], $unit['current_level']);
-        return $unit;
-    }
-
-    public function getAttack(array $unit)
-    {
-        switch ($unit['class_id'])
-        {
-            case self::WARRIOR_CLASS_ID:
-                $unit_stats = $unit['str'];
-                break;
-            case self::ARCHER_CLASS_ID:
-                $unit_stats = $unit['agi'];
-                break;
-            case self::CLERIC_CLASS_ID:
-            case self::MAGE_CLASS_ID:
-                $unit_stats = $unit['int'];
-                break;
-            default:
-                throw new ClassIdNotFoundException();
-        }
-        $attack = ceil(($unit['current_level']/2) + $unit_stats);
-        return $attack;
-    }
-
-    public function getDefense($vit)
-    {
-        $defense = round(($vit/2) + max(($vit*0.3) * (($vit^2)/150) / 3));
-        return $defense;
-    }
-
-    public function getHealthPoint($vit, $current_level)
-    {
-        $hp = round((($vit*10) + $current_level) + $vit);
-        return $hp;
+        $unit = $db->row('SELECT * FROM player_units WHERE player_id = ? AND unit_id = ?', array($this->player->id, $unit_id));
+        $row['player'] = $this->player;
+        $unit = new Unit($row);
+        return $unit->get($row['unit_id']);
     }
 
     public function getUnitLeaderId()
     {
-        $db = DB::conn();
-        $unit_leader_id = $db->value('SELECT unit_leader_id FROM player WHERE id = ?', array(self::$player->id));
-        return $unit_leader_id;
-    }
-
-    public function getUnitLeaderSkill()
-    {
-        $unit_leader_id = $this->getUnitLeaderId();
-        $unit_leader = $this->getUnitById($unit_leader_id);
-        $db = DB::conn();
-        $unit_leader_skill = $db->row('SELECT * FROM unit_leader_skill WHERE id = ?', array($unit_leader->unit_leader_skill_id));
-        if (!$unit_leader_skill) {
-            return false;
+        if ($this->unit_leader_id) {
+            return $this->unit_leader_id;
         }
-        $unit_leader_skill['unit_leader_id'] = $unit_leader_id;
-        return $unit_leader_skill;
+        $db = DB::conn();
+        $unit_leader_id = $db->value('SELECT unit_leader_id FROM player WHERE id = ?', array($this->player->id));
+        return $unit_leader_id;
     }
 }
