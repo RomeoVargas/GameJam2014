5b4080654d0b583f77107a70cb5285f71ae9ede2
acsoliman <a.soliman@klab.com>
2014-12-17 09:18:43 +0800

    revised unit storage

--- app/models/unit_storage.php
+++ app/models/unit_storage.php
@@ -1,5 +1,5 @@
 <?php
-class UnitStorage extends AppModel
+class UnitStorage
 {
     private $player;
     private $units;
@@ -24,9 +24,11 @@ class UnitStorage extends AppModel
         $db = DB::conn();
         $units = array();
         $rows = $db->rows('SELECT unit_id FROM player_units WHERE player_id = ?', array($this->player->id));
-        foreach ($rows as $unit_id) {
-            $units[] = $this->getUnit($unit_id);
+        foreach ($rows as $row) {
+            $units[] = $this->getUnit($row['unit_id']);
         }
+        $this->units=$units;
+
         return $units;
     }
 
